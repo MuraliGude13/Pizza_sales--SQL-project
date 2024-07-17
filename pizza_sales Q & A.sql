82067efc368895728095c4e8a6ae6b374617e491
Murali chowdary Gude <93189395+MuraliGude13@users.noreply.github.com>
2024-07-17 20:31:51 +0530

    Update pizza_sales Q & A.sql

--- pizza_sales Q & A.sql
+++ pizza_sales Q & A.sql
@@ -48,7 +48,9 @@ select pizza_types.name, sum(order_details.quantity + pizzas.price) as revenue
 from pizza_types join pizzas on pizzas.pizza_type_id = pizza_types.pizza_type_id 
 join order_details on order_details.pizza_id = pizzas.pizza_id group by pizza_types.name
  order by revenue desc limit 3;
- 
+
+
+-- ADVANCED
  -- Calculate the percentage contribution of each pizza type to total revenue.
  
 SELECT 
